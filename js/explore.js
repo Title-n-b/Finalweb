62757be7f238c2cb7ec3d9f0fc2c693d80a2ebaf
nonthakan <nontakantle25@gmail.com>
2024-12-11 02:23:14 +0700

    เพิ่มหน้าsavedกับdropdownหน้าexplorที่ยังบัคอยู่

--- js/explore.js
+++ js/explore.js
@@ -6,3 +6,29 @@ document.addEventListener('DOMContentLoaded', function() {
         exploreProductsContainer.innerHTML += createProductCard(product);
     });
 });
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const brand = dropdown.querySelector('.brand');
+    const option = dropdown.querySelectorAll('.brand li');
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        brand.classList.toggle('brand-open');
+    })
+    option.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            select.classList.remove('selected-clicked');
+            caret.classList.remove('caret-rotate');
+            brand.classList.remove('menu-open');
+            option.forEach(option => {
+                option.classList.remove('active');
+            });
+            option.classList.add('active');
+        });
+    });
+});
