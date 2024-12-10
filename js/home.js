fbfd2ba6abb1970bf51355711a7d928300ec7ebc
nonthakan <nontakantle25@gmail.com>
2024-12-10 20:40:03 +0700

    home

--- js/home.js
+++ js/home.js
@@ -5,4 +5,15 @@ document.addEventListener('DOMContentLoaded', function() {
     products.forEach(product => {
         featuredProductsContainer.innerHTML += createProductCard(product);
     });
+    
+});
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize the carousel
+    var myCarousel = new bootstrap.Carousel(document.getElementById('carouselExampleCaptions'), {
+      interval: 3000, // Change slide every 3 seconds
+      wrap: true // Continue from the last slide to the first one
+    });
+  
+    // Start auto-sliding
+    myCarousel.cycle();
 });
